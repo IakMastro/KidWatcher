cb171aa21245ea53c0953cf461bb7b4672a0f004
Timothy Leonard Stefaniak <timosste@yahoo.co.uk>
2021-06-30 23:57:47 +0300

    Quickfix

--- app/src/main/java/com/example/kidwatcher/MainActivity.java
+++ app/src/main/java/com/example/kidwatcher/MainActivity.java
@@ -18,48 +18,49 @@ import android.os.Bundle;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
-    @RequiresApi(api = Build.VERSION_CODES.P)
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+public class MainActivity extends AppCompatActivity
+{
+	@RequiresApi(api = Build.VERSION_CODES.P)
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        int PERMISSIONS_CHECK = 1;
-        String[] PERMISSIONS = {
-                Manifest.permission.RECEIVE_BOOT_COMPLETED,
-                Manifest.permission.RECEIVE_SMS,
-                Manifest.permission.READ_SMS,
-                Manifest.permission.READ_PHONE_STATE,
-                Manifest.permission.READ_CALL_LOG,
-                Manifest.permission.FOREGROUND_SERVICE
-        };
-        checkPermissions(PERMISSIONS, PERMISSIONS_CHECK);
+		int PERMISSIONS_CHECK = 1;
+		String[] PERMISSIONS = {Manifest.permission.RECEIVE_BOOT_COMPLETED, Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS, Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_CALL_LOG, Manifest.permission.FOREGROUND_SERVICE};
+		checkPermissions(PERMISSIONS, PERMISSIONS_CHECK);
 
-        startService(new Intent(this, TrackerService.class));
-    }
+		startService(new Intent(this, TrackerService.class));
+	}
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        finish();
-    }
+	@Override
+	public void onBackPressed()
+	{
+		super.onBackPressed();
+		finish();
+	}
 
-    public void checkPermissions(String[] permissions, int permissions_check) {
-        if (!permissionExists(this, permissions)) {
-            ActivityCompat.requestPermissions(this, permissions, permissions_check);
-        }
-    }
+	public void checkPermissions(String[] permissions, int permissions_check)
+	{
+		if (!permissionExists(this, permissions))
+		{
+			ActivityCompat.requestPermissions(this, permissions, permissions_check);
+		}
+	}
 
-    public static boolean permissionExists(Context context, String... permissions) {
-        if (context != null && permissions != null) {
-            for (String permission : permissions) {
-                if (ActivityCompat.checkSelfPermission(context, permission)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+	public static boolean permissionExists(Context context, String... permissions)
+	{
+		if (context != null && permissions != null)
+		{
+			for (String permission : permissions)
+			{
+				if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED)
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
--- app/src/main/java/com/example/kidwatcher/TrackerService.java
+++ app/src/main/java/com/example/kidwatcher/TrackerService.java
@@ -14,43 +14,49 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class TrackerService extends Service {
-    private DBHandler databaseHandler;
-    @SuppressLint("SimpleDateFormat")
-    private final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+public class TrackerService extends Service
+{
+	private DBHandler databaseHandler;
+	@SuppressLint("SimpleDateFormat")
+	private final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
+	@Nullable
+	@Override
+	public IBinder onBind(Intent intent)
+	{
+		return null;
+	}
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        databaseHandler = new DBHandler();
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId)
+	{
+		databaseHandler = new DBHandler();
 
-        LocalBroadcastManager.getInstance(this)
-                .registerReceiver(receiver, new IntentFilter("sms_sender"));
-        LocalBroadcastManager.getInstance(this)
-                .registerReceiver(receiver, new IntentFilter("phone"));
+		LocalBroadcastManager.getInstance(this).registerReceiver(receiver, new IntentFilter("sms_sender"));
+		LocalBroadcastManager.getInstance(this).registerReceiver(receiver, new IntentFilter("phone"));
 
-        return super.onStartCommand(intent, flags, startId);
-    }
+		return super.onStartCommand(intent, flags, startId);
+	}
 
-    BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String operation = intent.getStringExtra("operation");
-            String date = formatter.format(new Date());
-            String message = intent.getStringExtra("message");
-            String number = intent.getStringExtra("number");
+	BroadcastReceiver receiver = new BroadcastReceiver()
+	{
+		@Override
+		public void onReceive(Context context, Intent intent)
+		{
+			String operation = intent.getStringExtra("operation");
+			String date = formatter.format(new Date());
+			String message = intent.getStringExtra("message");
+			String number = intent.getStringExtra("number");
 
-            if (operation.equals("SMS")) {
-                databaseHandler.keepSMSLogs("incoming", date, message, number);
-                // TODO: "sent" sms
-            } else if (operation.equals("Phone")) {
-                databaseHandler.keepPhoneLogs("type", date, message, number);
-            }
-        }
-    };
+			if (operation.equals("SMS"))
+			{
+				databaseHandler.keepSMSLogs("incoming", date, message, number);
+				// TODO: "sent" sms
+			}
+			else if (operation.equals("Phone"))
+			{
+				databaseHandler.keepPhoneLogs("type", date, message, number);
+			}
+		}
+	};
 }
