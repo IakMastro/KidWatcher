8f87ecf2d5418da0dccd427470db5333923eba7c
Timothy Leonard Stefaniak <timosste@yahoo.co.uk>
2021-06-30 22:48:04 +0300

    Permissions

--- app/src/main/java/com/example/kidwatcher/MainActivity.java
+++ app/src/main/java/com/example/kidwatcher/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.kidwatcher;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
     private DBHandler databaseHandler;
     @SuppressLint("SimpleDateFormat")
     private final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
@@ -22,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int PERMISSIONS_CHECK = 1;
+        String[] PERMISSIONS = {
+                Manifest.permission.RECEIVE_BOOT_COMPLETED,
+                Manifest.permission.RECEIVE_SMS,
+                Manifest.permission.READ_SMS,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.READ_CALL_LOG
+        };
+        checkPermissions(this, PERMISSIONS, PERMISSIONS_CHECK);
 
         databaseHandler = new DBHandler();
     }
@@ -45,6 +59,23 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+
+    public void checkPermissions(MainActivity mainActivity, String[] permissions, int permissions_check){
+        if (!permissionExists(this, permissions)){
+            ActivityCompat.requestPermissions(this, permissions, permissions_check);
+        }
+    }
+
+    public static boolean permissionExists(Context context, String... permissions){
+        if (context != null && permissions != null){
+            for (String permission : permissions){
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     BroadcastReceiver receiver =  new BroadcastReceiver() {
